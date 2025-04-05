53c6f7d5e8047f1b1b5d7aa2d77785ff110107d5
Piyush <amburepiyush@gmail.com>
2025-04-05 21:01:11 +0530

    Add hospital_management_app/lib/firebase_config.dart 1743867070825

---
+++ hospital_management_app/lib/firebase_config.dart
@@ -0,0 +1,9 @@
+class FirebaseConfig {
+  static const String apiKey = 'YOUR_API_KEY';
+  static const String appId = 'YOUR_APP_ID';
+  static const String messagingSenderId = 'YOUR_MESSAGING_SENDER_ID';
+  static const String projectId = 'YOUR_PROJECT_ID';
+  static const String authDomain = 'YOUR_AUTH_DOMAIN';
+  static const String storageBucket = 'YOUR_STORAGE_BUCKET';
+  static const String measurementId = 'YOUR_MEASUREMENT_ID';
+}
